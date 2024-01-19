82ba182d38d6ef3e122c6e8b6a07eb768716b13f
Dipansu <dipansukapil143@gmail.com>
2024-01-19 20:06:44 +0530

    adding JWT

--- src/main/java/com/userservice/Services/AuthService.java
+++ src/main/java/com/userservice/Services/AuthService.java
@@ -1,11 +1,16 @@
 package com.userservice.Services;
 
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import javax.crypto.SecretKey;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,6 +26,9 @@ import com.userservice.models.Session;
 import com.userservice.models.SessionStatus;
 import com.userservice.repositories.SessionRepository;
 import com.userservice.repositories.UserRepository;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 
 @Service
 public class AuthService {
@@ -50,14 +58,41 @@ public class AuthService {
 //		 }
 		 
 		 if(!bCryptPasswordEncoder.matches(password, user.getPassword())) {
-			 return null;
+//			 return null;
+			 throw new RuntimeException("Wrong password entered");
 		 }
 		 
-		 String token = RandomStringUtils.randomAlphanumeric(30);
+//		 String token = RandomStringUtils.randomAlphanumeric(30);
+		 // Generating token
+		 //Create a test key suitable for the desired HMAC-SHA algorithm.
+		 
+		 MacAlgorithm alg = Jwts.SIG.HS256;
+		 SecretKey key = alg.key().build();
+		 
+//       String message = "{\n" +
+//       "  \"email\": \"harsh@scaler.com\",\n" +
+//       "  \"roles\": [\n" +
+//       "    \"student\",\n" +
+//       "    \"ta\"\n" +
+//       "  ],\n" +
+//       "  \"expiry\": \"31stJan2024\"\n" +
+//       "}";
+		 
+		 Map<String,Object> jsonMap = new HashMap<>();
+		 jsonMap.put("email", user.getEmail());
+		 jsonMap.put("roles", List.of(user.getRoles()));
+		 jsonMap.put("createdAt", new Date());
+		 jsonMap.put("expiryAt", DateUtils.addDays(new Date(), 30));
+		 
+		 String jws = Jwts.builder()
+				 .claims(jsonMap)
+				 .signWith(key,alg)
+				 .compact();
 		 
 		 Session session = new Session();
 		 session.setSessionStatus(SessionStatus.ACTIVE);
-		 session.setToken(token);
+//		 session.setToken(token);
+		 session.setToken(jws);
 		 session.setUser(user);
 		 sessionRepository.save(session);
 		 
@@ -66,7 +101,8 @@ public class AuthService {
 		 
 		 MultiValueMapAdapter<String, String> headers = new
 				 MultiValueMapAdapter<>(new HashMap<>());
-		 headers.add(HttpHeaders.SET_COOKIE, "auth-token:"+token);
+//		 headers.add(HttpHeaders.SET_COOKIE, "auth-token:"+token);
+		 headers.add(HttpHeaders.SET_COOKIE, "aiuth-token:"+jws);
 		
 		 ResponseEntity<UserDto> response = new ResponseEntity<>(userDto,headers,HttpStatus.OK);
 		 
